ebf2a165f83cc94ce7bc879e26965a09d03b6747
EthanDao4444 <ethandao4444@gmail.com>
2022-12-10 12:30:42 -0800

    Created first test files for AIthan, updated basic README.

---
+++ README.md
@@ -0,0 +1,3 @@
+To start:
+Run 'python3 -m venv .venv' on the command line, then 'source .venv/bin/activate'. Install the missing dependencies
+and run the program by running 'python3 AIthan.py' on the command line.
---
+++ aithan.py
@@ -0,0 +1,11 @@
+import numpy as np
+import speech_recognition as sr
+
+# Beginning of the AI
+class ChatBot():
+    def __init__(self, name):
+        print("----- starting up", name, "-----")
+        self.name = name
+# Execute the AI
+if __name__ == "__main__":
+    ai = ChatBot(name="AIthan")
